645e8b66995b5ed4d031c96f4cbcbc62d04b64e4
ENG24Ahmed <24ahmed11@gmail.com>
2026-03-25 18:48:45 +0300

    updatr

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:audio_session/audio_session.dart';
+import 'package:flutter/foundation.dart' show defaultTargetPlatform, TargetPlatform;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'screens/splash_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  if (defaultTargetPlatform == TargetPlatform.android) {
+    await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  }
   // ضروري لتشغيل الصوت من روابط الشبكة (مثل أذكار islamway) وتقليل التعارض مع مشغّل التلاوة
   final session = await AudioSession.instance;
   await session.configure(const AudioSessionConfiguration.music());
@@ -14,8 +18,33 @@ Future<void> main() async {
   runApp(const TartilApp());
 }
 
-class TartilApp extends StatelessWidget {
+class TartilApp extends StatefulWidget {
   const TartilApp({super.key});
+
+  @override
+  State<TartilApp> createState() => _TartilAppState();
+}
+
+class _TartilAppState extends State<TartilApp> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed &&
+        defaultTargetPlatform == TargetPlatform.android) {
+      SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
